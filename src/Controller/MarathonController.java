a45635040afc2706e7820c67310038b58749a4d2
Nagat Drawel <nag_drawel@hotmail.com>
2025-10-26 18:26:00 -0400

    initial commit

---
+++ src/Controller/MarathonController.java
@@ -0,0 +1,33 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class MarathonController {
+
+    @FXML
+    private Label runnerLabel;
+
+    @FXML
+    private Label distanceLabel;
+
+    @FXML
+    private Button playPauseButton;
+
+    @FXML
+    private Button speedUpButton;
+
+    @FXML
+    private Button speedDownButton;
+
+    @FXML
+    private Button resetButton;
+
+    @FXML
+    public void initialize() {
+        // TODO: Initialize model and setup UI components
+    }
+
+    // TODO: Add methods to handle button actions
+}
