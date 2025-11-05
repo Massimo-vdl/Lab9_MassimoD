fc17ab1e994c2d38fe38456ca4435f498408a43a
Massimo-vdl <massimo.deluca@videotron.ca>
2025-11-04 19:43:16 -0500

    First test commit

---
+++ Assignment2_Massimo/src/assignment2_massimo/Assignment2_Massimo.java
@@ -0,0 +1,17 @@
+
+package assignment2_massimo;
+
+/**
+ *
+ * @author massi
+ */
+public class Assignment2_Massimo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
